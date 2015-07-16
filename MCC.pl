f5913f32d2795775a248c68329fb48654f33a580
northeyt <northeyt@gmail.com>
2015-07-16 13:53:57 +0100

    add MCC.pl, a simple script for calculating MCC

---
+++ MCC.pl
@@ -0,0 +1,11 @@
+#!/acrm/usr/local/bin/perl
+
+@ARGV == 4 or die "Supply four numbers: TP, TN, FP, FN\n";
+my ($TP, $TN, $FP, $FN) = @ARGV;
+
+my $MCC
+    = ( ($TP * $TN) - ($FP * $FN) )
+    / sqrt ( ($TP+$FP)*($TP+$FN)*($TN+$FP)*($TN+$FN) );
+
+print $MCC . "\n";
+
