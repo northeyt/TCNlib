d441227a0c2e89a1f262faee2d6a3c54e8934483
northeyt <northeyt@gmail.com>
2015-08-27 18:12:49 +0100

    add option to specify unlabelled class label

--- bin/calcStatsFromWEKAOuputCSV.pl
+++ bin/calcStatsFromWEKAOuputCSV.pl
@@ -1,9 +1,12 @@
 #!/usr/bin/env perl
 use strict;
 use warnings;
-use WEKA;
+use TCNUtil::WEKA;
 use Carp;
 use Getopt::Long;
+
+my $undefLabel = "U";
+GetOptions("u=s", \$undefLabel);
 
 @ARGV == 3 or Usage();
 
@@ -12,15 +15,16 @@ my ($negLabel, $posLabel, $csv) = @ARGV;
 open(my $IN, "<", $csv) or die "Cannot open file $csv, $!";
 
 my $WEKA = WEKA->new(posLabel => $posLabel, negLabel => $negLabel,
-                     undefLabel => "U");
+                     undefLabel => $undefLabel);
 
 my $table = $WEKA->parseTableFromOutput([<$IN>]);
 $table->print_all();
 
 sub Usage {
     print <<EOF;
-$0 usage : negativeClass positiveClass CSV
-
+$0 usage : [-u undefLabel] negativeClass positiveClass CSV
+ undefLabel   : string that indicates a undefined class label.
+                DEFAULT = U
  negativeClass: negative class label (e.g. "no disease")
  positiveClass: positive class label (e.g. "disease")
 
