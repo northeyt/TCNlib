976b1a44c46f4412a7b76f82f6c2486b8b0dd6cd
northeyt <northeyt@gmail.com>
2016-05-24 11:42:32 +0100

    add p option

--- bin/getResidueRelASAs.pl
+++ bin/getResidueRelASAs.pl
@@ -3,11 +3,24 @@ use strict;
 use warnings;
 use pdb;
 use pdb::pdbFunctions;
+use pdb::get_files;
+use Getopt::Long;
+
+my $inputIsPqsCode = 0;
+GetOptions("p" => \$inputIsPqsCode);
+
+my $fileType = $inputIsPqsCode ? "pqs_file" : "pdb_file";
 
 while (<>) {
-    my $chain = chain->new(pdb_code => substr($_, 0, 4),
-                           chain_id => substr($_, 4, 1),
-                           solvent_cleanup => 1,);
+    chomp $_;
+    my $chainID = chop $_;
+    my $pdbCode = $_;
+    my $pdbFile = pdb::get_files->new(pdb_code => $pdbCode)->$fileType();
+    my $chain
+        = chain->new(pdb_code => $pdbCode, chain_id => $chainID,
+                     solvent_cleanup => 1, het_atom_cleanup => 1,
+                     pdb_file => $pdbFile
+                 );
     $chain->read_ASA();
     print join(":", ($chain->pdb_code, $chain->chain_id, substr($_, 2),
                      $chain->resid2RelASAHref->{$_}->{allAtoms})) . "\n"
