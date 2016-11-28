894dc80fc87f37adcf22c47b2037f3a05daa7136
northeyt <northeyt@gmail.com>
2016-11-28 10:43:05 +0000

    rASA file and rASA min can be supplied

--- bin/filterResLabelsByrASA.pl
+++ bin/filterResLabelsByrASA.pl
@@ -2,34 +2,63 @@
 use strict;
 use warnings;
 use Carp;
+use Getopt::Long;
+my $relASAFile;
+my $relASAMin;
+my $outFile;
 
-my $relASAMin = 10;
+GetOptions("r=s", \$relASAFile,
+           "o=s", \$outFile,
+           "m=s", \$relASAMin);
 
-@ARGV or die "Please supply a residue labels file";
-my $resLabelsFile = shift @ARGV;
-print "Getting residue relASAs: this can take a while if your data set is large ...\n";
-my @residueRelASAs = getResidueRelASAsSortedByResID($resLabelsFile);
-my @residueLabels  = getResidueLabelsSortedByResID($resLabelsFile);
-
-
-croak "Number of residue relASA values does not match number of residue labels"
-    if @residueRelASAs != @residueLabels;
-
-my $outFile = "relASAFiltered-min$relASAMin.residue.labels";
-
-print "Filtering residue labels by relASA\n";
+$relASAMin = 10 if ! defined $relASAMin;
+$outFile = "relASAFiltered-min$relASAMin.residue.labels" if ! defined $outFile;
 open(my $OUT, ">", $outFile) or die "Cannot open file $outFile, $!";
-for (my $i = 0 ; $i < @residueRelASAs ; ++$i) {
-    my $resIDAndRelASA = $residueRelASAs[$i];
-    my $resIDAndLabel  = $residueLabels[$i];
 
-    my ($firstResID, $relASA) = $resIDAndRelASA =~ /(.*):(.*)/;
-    my ($secndResID, $resLab) = $resIDAndLabel  =~ /(.*):(.*)/;
+@ARGV or Usage();
+my $resLabelsFile = shift @ARGV;
 
-    croak "Residue labels $firstResID and $secndResID do not match!"
-        if $firstResID ne $secndResID;
-
-    print {$OUT} "$resIDAndLabel\n" if $relASA > $relASAMin;
+if ($relASAFile) {
+    open(my $IN, "<", $relASAFile) or die "Cannot open file $relASAFile, $!";
+    my %resID2RelASA;
+    while (my $line = <$IN>) {
+        chomp $line;
+        my ($resID, $relASA) =  $line =~ /(.*):(.*)/;
+        $resID = uc(substr($resID, 0, 4)) . substr($resID, 4);
+        $resID2RelASA{$resID} = $relASA;
+    }
+    my @residueLabels = getResidueLabelsSortedByResID($resLabelsFile);
+    foreach my $resLabel (@residueLabels) {
+        my ($resID, $resLabel) =  $resLabel =~ /(.*):(.*)/;
+        if (! exists $resID2RelASA{$resID}) {
+            print {*STDERR} "WARNING: $resID has no relASA value!\n";
+            next;
+        }
+        my $relASA = $resID2RelASA{$resID};
+        print {$OUT} join(":", $resID, $resLabel) . "\n" if $relASA > $relASAMin;
+    }
+}
+else {
+    print "Getting residue relASAs: this can take a while if your data set is large ...\n";
+    my @residueRelASAs = getResidueRelASAsSortedByResID($resLabelsFile);
+    my @residueLabels  = getResidueLabelsSortedByResID($resLabelsFile);
+    
+    croak "Number of residue relASA values does not match number of residue labels"
+        if @residueRelASAs != @residueLabels;
+    
+    print "Filtering residue labels by relASA\n";
+    for (my $i = 0 ; $i < @residueRelASAs ; ++$i) {
+        my $resIDAndRelASA = $residueRelASAs[$i];
+        my $resIDAndLabel  = $residueLabels[$i];
+        
+        my ($firstResID, $relASA) = $resIDAndRelASA =~ /(.*):(.*)/;
+        my ($secndResID, $resLab) = $resIDAndLabel  =~ /(.*):(.*)/;
+        
+        croak "Residue labels $firstResID and $secndResID do not match!"
+            if $firstResID ne $secndResID;
+        
+        print {$OUT} "$resIDAndLabel\n" if $relASA > $relASAMin;
+    }
 }
 
 close $OUT;
@@ -45,4 +74,19 @@ sub getResidueLabelsSortedByResID {
     my $resLabelsFile = shift;
     my $sortedResLabels = `cat $resLabelsFile | sortResidues.sh`;
     return split(/\n/, $sortedResLabels);
+}
+
+sub Usage {
+    print <<EOF;
+$0 -r relASA-file -o out-file residue-labels-file
+
+Opts:
+    -r file containing residue relASA values. If not supplied, relASA values
+       will be calculated
+    -o output file. If not supplied, default of
+       relASAFiltered-min{MIN}.residue.labels will be used, where {MIN} is
+       relASA minimum.
+    -m relASA minimum used to filter residues. Default = 10
+EOF
+    exit(1);
 }
