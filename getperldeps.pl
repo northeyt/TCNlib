b1fecce9ab5c74c54276c90de31413e04fdbc02d
Tom Northey <northeyt@gmail.com>
2016-11-29 19:04:34 +0000

    get perl dependencies

---
+++ getperldeps.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/env perl
+use Module::Build;
+my @deps = qw(
+Moose
+MooseX::ClassAttribute
+IO::CaptureOutput
+Parallel::ForkManager
+Math::VectorReal
+Math::MatrixReal
+Statistics::PCA
+TryCatch
+LWP
+XML::DOM
+XML::Simple
+Test::MockObject::Extends
+LWP::Protocol::https
+);
+# Currently installed with versions specified - this can be
+# changed in the future if needs be
+my $build = Module::Build->new(
+    module_name => 'tcnutils',
+    requires => {map {$_ => 0} @deps},
+    );
+$build->dispatch('installdeps');
